3390d4beeaee7ab5f9dbcc2bff9700f10070f2cd
Wladimir Rocha <wld.rocha@gmail.com>
2025-07-07 14:46:08 -0400

    refactor: 🎨 Footer component to use <picture> for responsive logos

--- src/shared/components/footer/Footer.tsx
+++ src/shared/components/footer/Footer.tsx
@@ -7,8 +7,10 @@ const getCurrentYear = (): number => new Date().getFullYear()
 export const Footer = () => {
   return (
     <footer className='rm_footer-container'>
-      <img src={LogoMovil} alt='img-logo-white-movil' className='rm_footer-container__logo-movil' />
-      <img src={LogoDesktop} alt='img-logo-white-desktop' className='rm_footer-container__logo-desktop' />
+      <picture>
+        <source srcSet={LogoDesktop} media='(min-width: 768px)' />
+        <img src={LogoMovil} alt='Logo RIMAC' className='rm_footer-container__logo' />
+      </picture>
       <div className='rm_footer-container__separator' />
       <span className='rm_footer-container__description'>© {getCurrentYear()} RIMAC Seguros y Reaseguros.</span>
     </footer>
