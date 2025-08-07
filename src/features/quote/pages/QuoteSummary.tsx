62e52868f654e3dff9716a7dffa5919cb3bf5625
Wladimir Rocha <wld.rocha@gmail.com>
2025-08-07 17:31:32 -0400

    feat: ✨ add QuoteSummary component with detailed insurance summary and styles

--- src/features/quote/pages/QuoteSummary.tsx
+++ src/features/quote/pages/QuoteSummary.tsx
@@ -1,8 +1,32 @@
+import { Card } from '@/shared/components'
 import { InternalLayout } from '../layout/InternalLayout'
+import '../styles/quote-summary.scss'
+import { PersonsIcon } from '@/shared/components/icons/PersonsIcons'
+
 
 export const QuoteSummary = () => (
   <InternalLayout>
-    <div>Resumen del seguro</div>
-    
+    <div className='rm_quote-summary'>
+      <h1 className='rm_quote-summary__title'>Resumen del seguro</h1>
+      <Card className='rm_quote-summary__card'>
+        <header>
+          <h2 className='rm_quote-summary-card__title'>Precios calculados para:</h2>
+          <h3 className='rm_quote-summary-card__subtitle'>
+            <PersonsIcon /> Rocio Miranda Díaz
+          </h3>
+        </header>
+        <hr className='rm_quote-summary-card__separator' />
+        <section className='rm_quote-summary-card__section'>
+          <h4 className='rm_quote-summary-card__section-title'>Responsable de pago</h4>
+          <p className='rm_quote-summary-card__info-item'>DNI: 44488888</p>
+          <p className='rm_quote-summary-card__info-item'>Celular: 513021647</p>
+        </section>
+        <section className='rm_quote-summary-card__section'>
+          <h4 className='rm_quote-summary-card__section-title'>Plan elegido</h4>
+          <p className='rm_quote-summary-card__plan-name'>Plan en Casa y Clínica</p>
+          <p className='rm_quote-summary-card__plan-cost'>Costo del Plan: $99 al mes</p>
+        </section>
+      </Card>
+    </div>
   </InternalLayout>
 )
