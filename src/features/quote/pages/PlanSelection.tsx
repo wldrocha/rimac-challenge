42a35497586466d08875459b5a4c8341a455588b
Wladimir Rocha <wld.rocha@gmail.com>
2025-08-08 00:14:36 -0400

    feat: ✨ update QuoteSummary to display user details and selected plan information

--- src/features/quote/pages/PlanSelection.tsx
+++ src/features/quote/pages/PlanSelection.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { PlanSelectionCard } from '../components/plan-selection-card/PlanSelectionCard'
 import { PersonalIcon } from '@/shared/components/icons/PersonalIcon'
 import { OtherPersonIcon } from '@/shared/components/icons/OtherPersonIcon'
@@ -6,10 +8,47 @@ import { PlanDetailsCard } from '../components/plan-details-card/PlanDetailsCard
 import { InternalLayout } from '../layout/InternalLayout'
 import { useQuoteContext } from '../context/useQuoteContext'
 import '../styles/plan-selection.scss'
+import type { Plan } from '@/shared/services/types'
+
+type PlanType = 'self' | 'other'
+
+const planTypes = [
+  {
+    id: 1,
+    type: 'self' as PlanType,
+    title: 'Para mi',
+    description: 'Cotiza tu seguro de salud y agrega familiares si así lo deseas.',
+    icon: <PersonalIcon />,
+    hasDiscount: false
+  },
+  {
+    id: 2,
+    type: 'other' as PlanType,
+    title: 'Para alguien más',
+    description: 'Realiza una cotización para uno de tus familiares o cualquier persona.',
+    icon: <OtherPersonIcon />,
+    hasDiscount: true
+  }
+]
 
 export const PlanSelection = () => {
-  const { user, plans } = useQuoteContext()
+  const { user, plans, handleUpdateUser } = useQuoteContext()
+  const [selectedOption, setSelectedOption] = useState<PlanType | null>(null)
+  const navigate = useNavigate()
+  const hasDiscount = selectedOption === 'other'
+
   const filteredPlans = filterPlansByAge(plans, user?.age)
+
+  const getDiscountedPrice = (price: number, hasDiscount: boolean) => {
+    return hasDiscount ? Math.round(price * 0.95 * 100) / 100 : price
+  }
+
+  const handleNavigateSummary = () => navigate('/summary')
+
+  const handlePlanSelect = (plan: Plan) => {
+    handleUpdateUser({ plan })
+    handleNavigateSummary()
+  }
 
   return (
     <InternalLayout>
@@ -17,28 +56,36 @@ export const PlanSelection = () => {
         <h2 className='rm_plan_selection_title'>Rocío ¿Para quién deseas cotizar?</h2>
         <p className='rm_plan_selection_description'>Selecciona la opción que se ajuste más a tus necesidades.</p>
         <div className='rm_plan_selection_cards'>
-          <PlanSelectionCard
-            title='Para mi'
-            description='Cotiza tu seguro de salud y agrega familiares si así lo deseas.'
-            icon={<PersonalIcon />}
-          />
-          <PlanSelectionCard
-            title='Para alguien más'
-            description='Realiza una cotización para uno de tus familiares o cualquier persona.'
-            icon={<OtherPersonIcon />}
-          />
-        </div>
-        <div className='rm_plan_selection_list'>
-          {filteredPlans.map((plan, index) => (
-            <PlanDetailsCard
-              key={`plan-${index}-${plan.name}`}
-              name={plan.name}
+          {planTypes.map((plan) => (
+            <PlanSelectionCard
+              key={plan.id}
+              title={plan.title}
               description={plan.description}
-              price={plan.price}
-              onClick={() => console.log(`${plan.name} selected`)}
+              icon={plan.icon}
+              isSelected={selectedOption === plan.type}
+              onClick={() => setSelectedOption(selectedOption === plan.type ? null : plan.type)}
             />
           ))}
         </div>
+        {selectedOption && (
+          <div className='rm_plan_selection_list'>
+            {filteredPlans.map((plan, index) => {
+              const price = getDiscountedPrice(plan.price, hasDiscount)
+              return (
+                <PlanDetailsCard
+                  key={`plan-${index}-${plan.name}`}
+                  name={plan.name}
+                  description={plan.description}
+                  originalPrice={hasDiscount ? plan.price : undefined}
+                  price={price}
+                  onClick={() =>
+                    handlePlanSelect({ ...plan, price, originalPrice: hasDiscount ? plan.price : undefined })
+                  }
+                />
+              )
+            })}
+          </div>
+        )}
       </div>
     </InternalLayout>
   )
--- src/features/quote/pages/QuoteSummary.tsx
+++ src/features/quote/pages/QuoteSummary.tsx
@@ -1,32 +1,36 @@
 import { Card } from '@/shared/components'
-import { InternalLayout } from '../layout/InternalLayout'
-import '../styles/quote-summary.scss'
 import { PersonsIcon } from '@/shared/components/icons/PersonsIcons'
+import { InternalLayout } from '../layout/InternalLayout'
+import { useQuoteContext } from '../context/useQuoteContext'
+import '../styles/quote-summary.scss'
 
+export const QuoteSummary = () => {
+  const { user } = useQuoteContext()
 
-export const QuoteSummary = () => (
-  <InternalLayout>
-    <div className='rm_quote-summary'>
-      <h1 className='rm_quote-summary__title'>Resumen del seguro</h1>
-      <Card className='rm_quote-summary__card'>
-        <header>
-          <h2 className='rm_quote-summary-card__title'>Precios calculados para:</h2>
-          <h3 className='rm_quote-summary-card__subtitle'>
-            <PersonsIcon /> Rocio Miranda Díaz
-          </h3>
-        </header>
-        <hr className='rm_quote-summary-card__separator' />
-        <section className='rm_quote-summary-card__section'>
-          <h4 className='rm_quote-summary-card__section-title'>Responsable de pago</h4>
-          <p className='rm_quote-summary-card__info-item'>DNI: 44488888</p>
-          <p className='rm_quote-summary-card__info-item'>Celular: 513021647</p>
-        </section>
-        <section className='rm_quote-summary-card__section'>
-          <h4 className='rm_quote-summary-card__section-title'>Plan elegido</h4>
-          <p className='rm_quote-summary-card__plan-name'>Plan en Casa y Clínica</p>
-          <p className='rm_quote-summary-card__plan-cost'>Costo del Plan: $99 al mes</p>
-        </section>
-      </Card>
-    </div>
-  </InternalLayout>
-)
+  return (
+    <InternalLayout>
+      <div className='rm_quote-summary'>
+        <h1 className='rm_quote-summary__title'>Resumen del seguro</h1>
+        <Card className='rm_quote-summary__card'>
+          <header>
+            <h2 className='rm_quote-summary-card__title'>Precios calculados para:</h2>
+            <h3 className='rm_quote-summary-card__subtitle'>
+              <PersonsIcon /> {`${user?.name} ${user?.lastName}`}
+            </h3>
+          </header>
+          <hr className='rm_quote-summary-card__separator' />
+          <section className='rm_quote-summary-card__section'>
+            <h4 className='rm_quote-summary-card__section-title'>Responsable de pago</h4>
+            <p className='rm_quote-summary-card__info-item'>DNI: {user?.dni}</p>
+            <p className='rm_quote-summary-card__info-item'>Celular: {user?.phone}</p>
+          </section>
+          <section className='rm_quote-summary-card__section'>
+            <h4 className='rm_quote-summary-card__section-title'>Plan elegido</h4>
+            <p className='rm_quote-summary-card__plan-name'>{user?.plan?.name}</p>
+            <p className='rm_quote-summary-card__plan-cost'>Costo del Plan: ${user?.plan?.price} al mes</p>
+          </section>
+        </Card>
+      </div>
+    </InternalLayout>
+  )
+}
